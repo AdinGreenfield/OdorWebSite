5ec999c36f10e19dd4684422343e5c94ece1c14c
Adin Greenfield <adin@xfields.net>
2017-09-29 18:58:09 -0400

    Adding ruby on rails application

---
+++ odor_website/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+OdorWebsite::Application.config.secret_key_base = '09f42bf2a2689df8f450511577c1c0c7d4abbfed0281315db6245ac24ab583147825722223ccdbeef06ec65e5e89d433ae922d46b4e8d1dd1043889a92ad59b4'
